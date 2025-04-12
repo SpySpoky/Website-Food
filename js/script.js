8ed5d260c3c808fbeb4cb0e01aea122b1e08a81c
Artem Titov <TETOV1158@mail.ru>
2025-04-12 16:17:51 +0300

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,44 @@
+"use script"
+
+window.addEventListener('DOMContentLoaded', () => {
+
+    const tabs = document.querySelectorAll('.tabheader__item');
+    const tabsContent = document.querySelectorAll('.tabcontent');
+    const tabsParents = document.querySelector('.tabheader__items');
+
+    function hideTabContent() {
+        tabsContent.forEach(item => {
+            //item.style.display = 'none';
+            item.classList.add('hide');
+            item.classList.remove('show', 'fade');
+        });
+
+        tabs.forEach(item => {
+            item.classList.remove('tabheader__item_active');
+        });
+    }
+
+    function showTabContent(i = 0) {
+        tabs[i].classList.add('tabheader__item_active');
+        //tabsContent[i].style.display = 'block';
+        tabsContent[i].classList.add('show', 'fade');
+        tabsContent[i].classList.remove('hide');
+
+    }
+
+    tabsParents.addEventListener('click', (e) => {
+        const target = e.target;
+        if (target && target.classList.contains('tabheader__item')) {
+            tabs.forEach((item, i) => {
+                if (target === item) {
+                    hideTabContent();
+                    showTabContent(i);
+                }
+            });
+        }
+    });
+
+
+    hideTabContent();
+    showTabContent();
+});
